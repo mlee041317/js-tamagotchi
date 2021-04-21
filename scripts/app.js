09d732fb835814956ada2ebc772ff2d315a01ac6
mlee041317 <michelleleefit@gmail.com>
2021-04-20 19:00:07 -0700

    finished wireframe/userstory, commented out what needs to be coded in tomorrow

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+// const tamagotchi = {
+//     name: 'lilo',
+//     hunger: Math.floor(Math.random() * 10),
+//     sleepiness: Math.floor(Math.random() * 10),
+//     boredom: Math.floor(Math.random() * 10),
+//     age: 1
+// }
+// console.log(tamagotchi);
+
+const tamagotchi = {
+    name: 'Lilo',
+    happy: Math.floor(Math.random() * 10),
+    sleepiness: Math.floor(Math.random() * 10),
+    hunger: Math.floor(Math.random() * 10),
+    age: 1
+}
