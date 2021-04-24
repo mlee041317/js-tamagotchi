a2dd2539f735feaaef5cb6041639a861e64e4bf8
mlee041317 <michelleleefit@gmail.com>
2021-04-23 22:16:14 -0700

    update levels of hunger, sleepiness, and happiness to decrease when button is clicked, updated css, and html

--- scripts/app.js
+++ scripts/app.js
@@ -3,27 +3,36 @@
 const nameInput = document.getElementById('name');
 document.querySelector('.form').addEventListener('submit', function (e) {
     e.preventDefault();
-    alert(`${nameInput.value} is born!`);
+    alert(`Yay! ${nameInput.value} is born!`);
     let $name = $('#name').val();
     $('#openingScreen').addClass('hidden')
     $('#mainScreen').removeClass('hidden')
 
     $('#continueMessage').append(`${nameInput.value} wants to go feed Pudge! Click 'play' to continue`)
-
 });
 
-//When play is clicked, hide playScreen and bring in 
+
+//     const audio = new Audio('./imgs/notuna.mp3')
+//     audio.play();
+
+//     setTimeout(function () {
+//         audio.pause();
+//         audio.currentTime = 14;
+//     }, 19000);
+// }, 1000);
+
+//When play is clicked, hide playScreen
 $('.play').on('click', function () {
     $('main').addClass('hidden')
     $('#playScreen').removeClass('hidden')
 
 });
 
-//Create class for lilogotchi that will randomly produce hunger, sleepiness, and happiness levels
+// Create class for lilogotchi that will randomly produce hunger, sleepiness, and happiness levels
 const lilogotchi = {
-    hunger: Math.floor(Math.random() * 10),
-    sleepiness: Math.floor(Math.random() * 10),
-    happiness: Math.floor(Math.random() * 10)
+    hunger: Math.floor(Math.random() * 20),
+    sleepiness: Math.floor(Math.random() * 20),
+    happiness: Math.floor(Math.random() * 20)
 }
 
 function updateStats() {
@@ -33,28 +42,26 @@ function updateStats() {
     $('.age').text(`Age: ${lilogotchi.age}`)
 }
 
-
-
 const timer = setInterval(function () {
     console.log('tick')
 
-    // lilogotchi.hunger++;
-    // lilogotchi.sleepiness++;
-    // lilogotchi.happiness++;
+    lilogotchi.hunger++;
+    lilogotchi.sleepiness++;
+    lilogotchi.happiness++;
 
     updateStats();
     console.log(lilogotchi)
 
 
-    if (lilogotchi.hunger === 10) {
+    if (lilogotchi.hunger === 20) {
         death();
         clearInterval(timer);
     }
-    else if (lilogotchi.sleepiness === 10) {
+    else if (lilogotchi.sleepiness === 20) {
         death();
         clearInterval(timer);
     }
-    else if (lilogotchi.happiness === 10) {
+    else if (lilogotchi.happiness === 20) {
         death();
         clearInterval(timer);
     }
@@ -62,14 +69,20 @@ const timer = setInterval(function () {
 
 }, 10000);
 
-//if lilogotchi dies, deathScreen is going to show up
+// if lilogotchi dies, deathScreen is going to show up
 function death() {
     $('main').addClass('hidden')
     $('#deathScreen').removeClass('hidden')
     $('#death-message').text('Oh no! Your lilogotchi died!')
-    const audio = new Audio('./imgs/nice-ocean-waves.mp3')
+
+    const audio = new Audio('./imgs/rickroll.mp3')
     audio.play();
 
+    $('.home').on('click', function () {
+        $('main').addClass('hidden')
+        $('#mainScreen').removeClass('hidden')
+
+    });
 }
 
 //playScreen
@@ -78,14 +91,12 @@ $('.feed-pudge').on('click', function () {
     $('#lilo-swimming').attr("src", "https://i.ibb.co/mh804w8/pudge-the-fish.gif");
     //Alert message will pop up after feed pudge button
     $('#naniMessage').append('Time to come home for lunch!')
-    // setTimeout(function () {
-    //     alert('TXT MSG- Nani: Time to come home for lunch!');
-    // }, 1000);
+
 
 });
 //Listen for a click on Lunch
 $('.lunch').on('click', function () {
-    //add in decrease level of hunger
+    updateStats(lilogotchi.hunger--)
     $('main').addClass('hidden')
     $('#lunchScreen').removeClass('hidden')
 
@@ -97,24 +108,21 @@ $('#lunchMessage').append('But I\'m not hungry! I need a nap.')
 
 //Listen for a click on "sleep" button
 $('.sleepiness').on('click', function () {
-    //add in decrease level of sleepiness
+    updateStats(lilogotchi.sleepiness--)
     $('main').addClass('hidden')
     $('#sleepScreen').removeClass('hidden')
 
+    const audio = new Audio('./imgs/leavemealone.mp3')
+    audio.play();
 });
 
-
-//sleepScreen
-//Insert "Leave me alone to die" song https://www.youtube.com/watch?v=yb1m04zid8k
-//Move to mainScreen
 $('.home').on('click', function () {
-    //add in decrease level of sleepiness
+    updateStats(lilogotchi.happiness--)
     $('main').addClass('hidden')
     $('#mainScreen').removeClass('hidden')
 
 });
 
-//deathScreen
 
 
 
