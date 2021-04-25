2f597bf4b29fdf34accf4973612b2d7beb1c3b12
mlee041317 <michelleleefit@gmail.com>
2021-04-25 13:24:29 -0700

    Replaced all images of lilo with stitch, adjusted css, and added in more audio to js

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,41 @@
-//openingScreen
-//When player enters name, an alert will pop up, console will log name, and openingScreen will disappear when submit is clicked
+//Create a variable that will store the value when the user enters a name into the input
+//Select the form that is storing both the input and submit button and add an eventListener to the submit button
+//Once the submit button is clicked, display an alert that will display stored value name and a message
+//Add class of hidden to the openingScreen and remove class of hidden to mainScreen
+//Append a message to the mainScreen that lets the user know their tamagotchi would like to play
+const audio = new Audio('./imgs/animewow.mp3')
+audio.play();
+
 const nameInput = document.getElementById('name');
 document.querySelector('.form').addEventListener('submit', function (e) {
     e.preventDefault();
+    const audio = new Audio('./imgs/histitch.mp3')
+    audio.play();
     alert(`Yay! ${nameInput.value} is born!`);
     let $name = $('#name').val();
+
+
     $('#openingScreen').addClass('hidden')
     $('#mainScreen').removeClass('hidden')
 
-    $('#continueMessage').append(`${nameInput.value} wants to go feed Pudge! Click 'play' to continue`)
+    $('#continueMessage').append(`${nameInput.value} wants to go feed Pudge!`)
 });
 
 
-//     const audio = new Audio('./imgs/notuna.mp3')
-//     audio.play();
-
-//     setTimeout(function () {
-//         audio.pause();
-//         audio.currentTime = 14;
-//     }, 19000);
-// }, 1000);
-
-//When play is clicked, hide playScreen
+//When the play button is clicked, hide playScreen
 $('.play').on('click', function () {
     $('main').addClass('hidden')
     $('#playScreen').removeClass('hidden')
-
 });
 
-// Create class for lilogotchi that will randomly produce hunger, sleepiness, and happiness levels
+//Create a class for the tamagotchi that will store hunger, sleepiness, happiness
 const lilogotchi = {
     hunger: Math.floor(Math.random() * 20),
     sleepiness: Math.floor(Math.random() * 20),
     happiness: Math.floor(Math.random() * 20)
 }
 
+//Create a function that will update the meters throughout the game
 function updateStats() {
     $('.hunger').attr('value', `${lilogotchi.hunger}`)
     $('.sleepiness').attr('value', `${lilogotchi.sleepiness}`)
@@ -42,6 +43,9 @@ function updateStats() {
     $('.age').text(`Age: ${lilogotchi.age}`)
 }
 
+//Create a variable that will store a function that updates the tamagotchi's health level every 10 seconds
+//If the tamagotchi hunger, sleepiness, or happiness level reaches 20 then the death screen will appear
+//Add in a button that will allow the user to start the game over
 const timer = setInterval(function () {
     console.log('tick')
 
@@ -69,7 +73,6 @@ const timer = setInterval(function () {
 
 }, 10000);
 
-// if lilogotchi dies, deathScreen is going to show up
 function death() {
     $('main').addClass('hidden')
     $('#deathScreen').removeClass('hidden')
@@ -85,28 +88,29 @@ function death() {
     });
 }
 
-//playScreen
-//Listen for a click on "feed pudge" button, when clicked bring in Pudge's gif
+//Listen for a click on the "feed pudge" button, when clicked a new gif will be displayed and an alert message will appear
+//Listen for a click on the "lunch" button and update the tamagotchi's hunger level, and remove class of hidden on lunchScreen
 $('.feed-pudge').on('click', function () {
-    $('#lilo-swimming').attr("src", "https://i.ibb.co/mh804w8/pudge-the-fish.gif");
-    //Alert message will pop up after feed pudge button
+    $('#stitch-bikini').attr("src", "https://i.ibb.co/mh804w8/pudge-the-fish.gif");
+    const audio = new Audio('./imgs/animewow.mp3')
+    audio.play();
     $('#naniMessage').append('Time to come home for lunch!')
 
-
 });
-//Listen for a click on Lunch
+
 $('.lunch').on('click', function () {
     updateStats(lilogotchi.hunger--)
     $('main').addClass('hidden')
     $('#lunchScreen').removeClass('hidden')
+    const audio = new Audio('./imgs/stitcheating.mp3')
+    audio.play();
 
 });
 
-//lunchScreen
-//add in <p> and .append "But I'm not hungry! I need a nap."
+//Add in an appending message
 $('#lunchMessage').append('But I\'m not hungry! I need a nap.')
 
-//Listen for a click on "sleep" button
+//Listen for a click on "sleep" button and update the tamagotchi's sleepiness level, and remove class of hidden of sleepScreen
 $('.sleepiness').on('click', function () {
     updateStats(lilogotchi.sleepiness--)
     $('main').addClass('hidden')
@@ -122,7 +126,3 @@ $('.home').on('click', function () {
     $('#mainScreen').removeClass('hidden')
 
 });
-
-
-
-
